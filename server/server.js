439f624149048bceaf3767e2468c88c3a898361f
AnjinIsmail <anjin.ismail@gmail.com>
2021-06-17 10:52:51 -0700

    server.js modified

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const app = express();
+const exp = require('express');
+const app = exp();
 
 
 const path = require('path');
-const express = require('express');
-const app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
+app.use(exp.static(publicPath));
 
 
 
@@ -15,7 +13,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
  });
 
- 
+
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}!`);
